b77410c9d18216b70826725757f3fa0a2b398156
Gian Pratama <pratama.zidan@gmail.com>
2022-03-19 14:23:27 +0700

    initial commit

--- demo/lib/03_text_widget.dart
+++ demo/lib/03_text_widget.dart
@@ -14,18 +14,18 @@ class TextWidget extends StatelessWidget {
           color: Colors.lightBlue,
           width: 150,
           height: 100,
-          child: Text(
-              'Saya Gian, dan sedang melatih kemampuan Flutter saya di HSI Sandbox',
-              maxLines: 4,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-              softWrap: false,
-              style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w700,
-                fontSize: 20,
-              )),
+          child:
+              Text('Saya sedang melatih kemampuan Flutter saya di HSI Sandbox',
+                  maxLines: 4,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  softWrap: false,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                  )),
         ),
       ),
     );
