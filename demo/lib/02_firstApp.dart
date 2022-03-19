ec24a0283364b3ed497a64e30d4fc75791b2574c
Gian Pratama <pratama.zidan@gmail.com>
2022-03-19 14:44:10 +0700

    update content

---
+++ demo/lib/02_firstApp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstApp extends StatelessWidget {
+  const FirstApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video 02 - Hello World'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
