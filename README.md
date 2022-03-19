40030b9a21c5e767430b0eaca6059b7abc623164
Gian Pratama <pratama.zidan@gmail.com>
2022-03-19 14:22:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter Sprint 1
+ Flutter Sprint 1 HSI Sandbox feat KodingWorks @ 19 Maret - 25 Maret 2022
---
+++ demo/lib/03_text_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video 03 - Text Widget'),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.lightBlue,
+          width: 150,
+          height: 100,
+          child: Text(
+              'Saya Gian, dan sedang melatih kemampuan Flutter saya di HSI Sandbox',
+              maxLines: 4,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              softWrap: false,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+              )),
+        ),
+      ),
+    );
+  }
+}
