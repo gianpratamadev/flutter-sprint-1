e4ee2f5d8e2ee3b77bedf84aef057a80a051672f
Gian Pratama <pratama.zidan@gmail.com>
2022-03-21 06:48:16 +0700

    update content

--- task1/lib/02_firstApp.dart
+++ task1/lib/02_firstApp.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FirstApp extends StatelessWidget {
-  const FirstApp({Key? key}) : super(key: key);
+  // const FirstApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- task1/lib/main.dart
+++ task1/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '10_animatedGesture.dart';
+import '02_firstApp.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimatedGestures(),
+      home: FirstApp(),
     );
   }
 }
